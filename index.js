c6ee1a7ea1575d28add0d624237ebf0c36926281
mdiibrahim <mdiibrahim549@gmail.com>
2022-10-27 18:00:01 +0600

    course name in side bar added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const courseName = require('./Courses/courses.json');
 const app = express();
+
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.get('/', (req, res) => {
     res.send("Code for Chilling server side is running");
+});
+app.get('/courses', (req, res) => {
+    res.send(courseName);
 });
 app.listen(port, () => {
     console.log(`Codde for Chill is running on ${port}`);
